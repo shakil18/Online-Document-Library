9eab09d12463d7122b978e9c73bd3ed78af528b4
shakil18 <shakil.wave@gmail.com>
2017-12-25 16:11:02 +0100

    configured

--- grproxy/src/grproxy/grproxy.go
+++ grproxy/src/grproxy/grproxy.go
@@ -1,5 +1,123 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"time"
+	"github.com/samuel/go-zookeeper/zk"
+)
 
+var urls []string
+
+func main() {
+	conn := connect()
+	defer conn.Close()
+
+	flags := int32(0)
+
+	for conn.State() != zk.StateHasSession {
+		fmt.Printf("Zookeeper is loading... \n")
+		time.Sleep(5)
+	}
+
+	acl := zk.WorldACL(zk.PermAll)
+
+	exists, stat, err := conn.Exists("/grproxy")
+	errHndlr(err)
+	fmt.Printf("exists: %+v %+v\n", exists, stat)
+
+	if !exists {
+		grproxy, err := conn.Create("/grproxy", []byte("grproxy:80"), flags, acl)
+		errHndlr(err)
+		fmt.Printf("create: %+v\n", grproxy)
+	}
+
+	childchn, errors := monitorGserver(conn, "/grproxy")
+
+	go func() {
+		for {
+			select {
+
+			case children := <-childchn:
+				fmt.Printf("%+v .....\n", children)
+				var temp []string
+				for _, child := range children {
+					gserve_urls, _, err := conn.Get("/grproxy/" + child)
+					temp = append(temp, string(gserve_urls))
+					if err != nil {
+						fmt.Printf("from child: %+v\n", err)
+					}
+				}
+				urls = temp
+				fmt.Printf("%+v \n", urls)
+			case err := <-errors:
+				fmt.Printf("%+v routine error \n", err)
+			}
+		}
+	}()
+
+	proxy := NewMultipleHostReverseProxy()
+	log.Fatal(http.ListenAndServe(":8080", proxy))
+
+}
+
+func connect() *zk.Conn {
+	zksStr := "zookeeper:2181"
+	zks := strings.Split(zksStr, ",")
+	conn, _, err := zk.Connect(zks, time.Second)
+	errHndlr(err)
+	return conn
+}
+
+func errHndlr(err error) {
+	if err != nil {
+		//panic(err)
+		fmt.Printf("%+v from errHndlr \n", err)
+	}
+}
+
+func monitorGserver(conn *zk.Conn, path string) (chan []string, chan error) {
+
+	servers := make(chan []string)
+	errors := make(chan error)
+	go func() {
+		for {
+			children, _, events, err := conn.ChildrenW(path)
+			if err != nil {
+				errors <- err
+				return
+			}
+			servers <- children
+			evt := <-events
+			if evt.Err != nil {
+				errors <- evt.Err
+				return
+			}
+		}
+	}()
+	return servers, errors
+}
+
+func NewMultipleHostReverseProxy() *httputil.ReverseProxy {
+	director := func(req *http.Request) {
+
+		if req.URL.Path == "/library" {
+			fmt.Println("This is for gserver")
+			target := urls[rand.Int()%len(urls)]
+			req.URL.Scheme = "http"
+			req.URL.Host = target
+
+		} else {
+
+			fmt.Println("This is for nginx")
+			req.URL.Scheme = "http"
+			req.URL.Host = "nginx"
+		}
+
+	}
+	return &httputil.ReverseProxy{Director: director}
 }
--- gserve/src/gserve/gserve.go
+++ gserve/src/gserve/gserve.go
@@ -1,5 +1,160 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+	"github.com/samuel/go-zookeeper/zk"
+)
+
+var zookeeper string = "zookeeper"
+var hbase_host string = "hbase"
+var server_name string = "Unknown"
+
 func main() {
 
+	server_name = os.Getenv("servername")
+	conn := connect()
+	defer conn.Close()
+
+	for conn.State() != zk.StateHasSession {
+		fmt.Printf(" %s (Server) is loading the Zookeeper... \n", server_name)
+		time.Sleep(30)
+	}
+
+        fmt.Printf(" %s (Server) is connected with Zookeeper\n", server_name)
+	flags := int32(zk.FlagEphemeral)
+	acl := zk.WorldACL(zk.PermAll)
+
+	gserv, err := conn.Create("/grproxy/"+server_name, []byte(server_name+":9091"), flags, acl)
+	errHndlr(err)
+	fmt.Printf("create ephemeral node: %+v\n", gserv)
+
+	startServer()
+}
+
+func connect() *zk.Conn {
+	zksStr := zookeeper + ":2181"
+	zks := strings.Split(zksStr, ",")
+	conn, _, err := zk.Connect(zks, time.Second)
+	errHndlr(err)
+	return conn
+}
+
+func errHndlr(err error) {
+	if err != nil {
+		//panic(err)
+		fmt.Printf("%+v From errHndlr \n", err)
+	}
+}
+
+func startServer() {
+	http.HandleFunc("/library", handler)
+	log.Fatal(http.ListenAndServe(":9091", nil))
+}
+
+func handler(writer http.ResponseWriter, req *http.Request) {
+
+	if req.Method == "POST" || req.Method == "PUT" {
+
+		encodedJsonByte, err := ioutil.ReadAll(req.Body)
+		errHndlr(err)
+
+		// get encoded data from []byte type
+		encodedJSON := encoder(encodedJsonByte)
+		fmt.Println("encodedJSON : ", string(encodedJSON))
+
+		req.Header.Set("Content-type", "application/json")
+		postHbase(encodedJSON)
+		fmt.Fprintf(writer, "an %s\n", "POST")
+
+	} else if req.Method == "GET" {
+		req.Header.Set("Accept", "application/json")
+		responseData := getHbase()
+		
+		fmt.Fprintf(writer, "Response from hbase:\n\n %s\n", string(responseData))
+
+	} else {
+		fmt.Fprintf(writer, "Invalid Request from Client")
+	}
+
+	fmt.Fprintf(writer, "proudly served by %s", server_name)
+
+}
+
+func encoder(unencodedJSON []byte) string {
+	// get go object from json byte
+	var unencodedRows RowsType
+	json.Unmarshal(unencodedJSON, &unencodedRows)
+
+	//  encode all fields value of go object , return EncRowsType
+	encodedRows := unencodedRows.encode()
+
+	// convert to json byte[] from go object (EncRowsType)
+	encodedJSON, _ := json.Marshal(encodedRows)
+
+	return string(encodedJSON)
+}
+
+func postHbase(encodedJSON string) {
+
+	req_url := "http://" + hbase_host + ":8080/se2:library/fakerow"
+
+	resp, err := http.Post(req_url, "application/json", bytes.NewBuffer([]byte(encodedJSON)))
+
+	if err != nil {
+		fmt.Println("Error from response: %+v", err)
+		return
+	}
+
+	fmt.Println("Post Response: ", resp.Status)
+	defer resp.Body.Close()
+}
+
+func getHbase() string {
+
+	req_url := "http://" + hbase_host + ":8080/se2:library/*"
+
+	// resp, getErr := http.Get(req_url)
+	req, _ := http.NewRequest("GET", req_url, nil)
+	req.Header.Set("Accept", "application/json")
+	client := &http.Client{}
+	resp, getErr := client.Do(req)
+	errHndlr(getErr)
+
+	fmt.Println("Get Response: ", resp.Status)
+
+	encodedJsonByte, err := ioutil.ReadAll(resp.Body)
+	errHndlr(err)
+
+	decodedJSON := decoder(encodedJsonByte)
+	defer resp.Body.Close()
+	return decodedJSON
+}
+
+func decoder(encodedJSON []byte) string {
+
+	// get go object from json byte
+	var encodedRows EncRowsType
+	fmt.Println("From decoder test print: ", string(encodedJSON))
+	json.Unmarshal(encodedJSON, &encodedRows)
+	fmt.Println("From decoder first: ", encodedRows)
+
+	//  decode all fields value of go object , return RowsType
+	decodedRows, err := encodedRows.decode()
+	if err != nil {
+		fmt.Println("%+v", err)
+	}
+	fmt.Println("From decoder second: ", decodedRows)
+	// convert to json byte[] from go object (RowsType)
+	deCodedJSON, _ := json.Marshal(decodedRows)
+
+	//fmt.Println("From decoder method: ", string(deCodedJSON))
+	return string(deCodedJSON)
 }
